6217455fd4a4937b53bc81b816a6452f6118b952
stergiossepardanis <stergios.separdanis@gmail.com>
2026-01-14 12:05:36 +0200

    Improve formatting and indentation in core_queries.sql

--- sql/core_queries.sql
+++ sql/core_queries.sql
@@ -1,41 +1,47 @@
 -- Core view of dimArea (geographic attributes)
-SELECT area_id,
-area_name as area, 
-lat,
-lon
+SELECT 
+    area_id,
+    area_name AS area,
+    lat,
+    lon
 FROM dbo.dimArea;
 
 -- Core view of dimCrime (crime classification attributes)
-SELECT crm_id,
-part_1_2, 
-crm_cd_desc as crime_type,
-status_desc as status
+SELECT 
+    crm_id,
+    part_1_2,
+    crm_cd_desc AS crime_type,
+    status_desc AS status
 FROM dbo.dimCrime;
 
 -- Core view of dimPremise (premise/location attributes)
-SELECT premis_id,
-premis_desc
+SELECT 
+    premis_id,
+    premis_desc
 FROM dbo.dimPremise;
 
 -- Core view of dimVictim (victim demographic attributes)
-SELECT vict_id,
-vict_age, 
-vict_sex,
-vict_descent
+SELECT 
+    vict_id,
+    vict_age,
+    vict_sex,
+    vict_descent
 FROM dbo.dimVictim;
 
 -- Core view of dimWeapon (weapon attributes)
-SELECT weapon_id
+SELECT 
+    weapon_id
 FROM dbo.dimWeapon;
 
 -- Core view of factCrime (incident-level fact table)
-SELECT dr_no,
-date_occ, 
-time_occ,
-DATEDIFF(DAY, date_occ, date_rptd) AS delay,
-area_id, 
-crm_id, 
-premis_id, 
-vict_id,
-weapon_id
+SELECT 
+    dr_no,
+    date_occ,
+    time_occ,
+    DATEDIFF(DAY, date_occ, date_rptd) AS delay,
+    area_id,
+    crm_id,
+    premis_id,
+    vict_id,
+    weapon_id
 FROM dbo.factCrime;
